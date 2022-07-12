11fc380a8215b59ada7f56d0e894303aa48bbbed
pcolor999 <chilladdmail@gmail.com>
2022-07-12 16:24:19 +0700

    2 files modified

--- src/index.js
+++ src/index.js
@@ -18,6 +18,14 @@ root.render(
       <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
         <Route path="invoices" element={<Invoices />}>
+          <Route
+            index
+            element={
+              <main style={{ padding: '1rem 0' }}>
+                <p>Select an invoice</p>
+              </main>
+            }
+          ></Route>
           <Route path=":invoiceId" element={<Invoice />}></Route>
         </Route>
         <Route
--- src/routes/invoices.js
+++ src/routes/invoices.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, NavLink } from 'react-router-dom';
 import { getInvoices } from '../data.js';
 
 export default function Invoices() {
@@ -8,13 +8,19 @@ export default function Invoices() {
     <div>
       <nav>
         {invoices.map((invoice) => (
-          <Link
-            style={{ display: 'block', margin: '1rem 0' }}
+          <NavLink
+            style={({ isActive }) => {
+              return {
+                display: 'block',
+                margin: '1rem 0',
+                color: isActive ? 'red' : '',
+              };
+            }}
             to={'/invoices/' + invoice.number}
             key={invoice.number}
           >
             {invoice.name}
-          </Link>
+          </NavLink>
         ))}
       </nav>
       <Outlet />

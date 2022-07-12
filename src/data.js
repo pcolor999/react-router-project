c287eab6a1cd3ca3c1d1f6f0d9857ebb2275c928
pcolor999 <chilladdmail@gmail.com>
2022-07-12 16:15:18 +0700

    2 files modified

--- src/data.js
+++ src/data.js
@@ -37,3 +37,7 @@ let invoices = [
 export function getInvoices() {
   return invoices;
 }
+
+export function getInvoice(number) {
+  return invoices.find((invoice) => invoice.number === number);
+}
--- src/routes/invoice.js
+++ src/routes/invoice.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { getInvoice } from '../data.js';
 
 export default function Invoice() {
   let params = useParams();
-  return <h2>Invoice {params.invoiceId}</h2>;
+  let invoiceNumber = parseInt(params.invoiceId, 10);
+  let invoice = getInvoice(invoiceNumber);
+  return (
+    <main>
+      <h2>Total Due: {invoice.amount}</h2>
+      <p>
+        {invoice.name}: {invoice.number}
+      </p>
+      <p>Due Date: {invoice.due}</p>
+    </main>
+  );
 }
